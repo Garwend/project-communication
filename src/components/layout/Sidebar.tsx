9ddacf7d9f7019d0816c2e210de004d60ccd16d5
Garwend <kasprzak000@gmail.com>
2023-04-27 23:10:03 +0200

    sidebar nav link

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,36 +1,11 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
 import { Home } from "lucide-react";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  navigationMenuTriggerStyle,
-} from "~/components/ui/navigation-menu";
+import { NavLink } from "../ui/nav-link";
 import List from "~/components/projects/List";
-import { cn } from "~/lib/utils";
 
 export default function Sidebar() {
-  const router = useRouter();
-
   return (
     <aside className="h-[calc(100vh-3rem)] w-52 overflow-auto border-r border-border bg-background p-2">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink
-                className={cn(navigationMenuTriggerStyle(), "hover:underline")}
-                active={router.pathname === "/"}
-              >
-                <Home className="mr-2 h-4 w-4" />
-                Strona główna
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <NavLink href="/" name="Stron główna" icon={Home} />
       <List />
     </aside>
   );
